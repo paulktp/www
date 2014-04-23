840d9b3c9acd2dbee83c4b0b4370cf6b5c42a117
paulktp <paul@ktp.net>
2014-04-23 16:38:45 +0200

    seg

--- coreTh.js
+++ coreTh.js
@@ -12,37 +12,7 @@
         var element = document.getElementById('geolocation');				
 		lati = position.coords.latitude;
 		longi = position.coords.longitude ;
-    }
-
-    // onError Callback receives a PositionError object
-    //
-    function onError(error) {
-        alert('code: '    + error.code    + '\n' +
-                'message: ' + error.message + '\n');
-    }
-	
-	var element1 = document.getElementById('DivIpad');
-	var element2 = document.getElementById('ret');
-	if(device.model.indexOf('iPad') != -1){
-		element1.innerHTML = '<img src="img/picRestMetz.jpg" style="width: 100%; height:38%; position:relative; bottom:0px"/>';
-		}
-	if(device.platform == "iOS"){
-			element2.innerHTML = '<p onClick = "redirectIndex()" style="margin-left:3%; margin-top:5%; color:black; text-decoration: underline;">Retour</p>';
-	}
-
-
-	function redirectIndex() {	
-         var ref = window.open('index.html', '_self', 'location=yes');
-	}
-	function redirectWebSite() {	
-         var ref = window.open('http://www.kyousushi.com', '_system', 'location=yes');
-	}
-	function redirectKyouFB() {	
-         var ref = window.open('https://www.facebook.com/KyouSushi', '_system', 'location=yes');
-	}	
-	
-	var map;
-$(document).ready(function(){
+			var map;
   var map = new GMaps({
     el: '#directions_map',
     lat: 49.357571,
@@ -82,4 +52,32 @@ $(document).ready(function(){
       infoWindow: {
         content: 'Restaurant kyou sushi de Metz Adresse : 6 Rue du Moyen Pont, 57000 Metz </ br>Téléphone : 03 87 66 83 52 </ br>Horaires : Ouvert 7/7j de 12:00 à 15:00 et de 18:30 à 23:00'}
     });
-});
+    }
+
+    // onError Callback receives a PositionError object
+    //
+    function onError(error) {
+        alert('code: '    + error.code    + '\n' +
+                'message: ' + error.message + '\n');
+    }
+	
+	var element1 = document.getElementById('DivIpad');
+	var element2 = document.getElementById('ret');
+	if(device.model.indexOf('iPad') != -1){
+		element1.innerHTML = '<img src="img/picRestMetz.jpg" style="width: 100%; height:38%; position:relative; bottom:0px"/>';
+		}
+	if(device.platform == "iOS"){
+			element2.innerHTML = '<p onClick = "redirectIndex()" style="margin-left:3%; margin-top:5%; color:black; text-decoration: underline;">Retour</p>';
+	}
+
+
+	function redirectIndex() {	
+         var ref = window.open('index.html', '_self', 'location=yes');
+	}
+	function redirectWebSite() {	
+         var ref = window.open('http://www.kyousushi.com', '_system', 'location=yes');
+	}
+	function redirectKyouFB() {	
+         var ref = window.open('https://www.facebook.com/KyouSushi', '_system', 'location=yes');
+	}	
+	
