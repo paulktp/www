c8928cc4309febf552641d0cc81309b5d55f6775
paulktp <paul@ktp.net>
2014-04-24 13:41:02 +0200

    wdrg

--- coreM.js
+++ coreM.js
@@ -9,6 +9,8 @@
 		var lati;
 		var longi;
     function onSuccess(position) {
+	 
+	var element1 = document.getElementById('DivIpad');
 	var element2 = document.getElementById('ret');
 	/*if(device.model.indexOf('iPad') != -1){
 		element1.innerHTML = '<img src="img/picRestMetz.jpg" style="width: 100%; height:38%; position:relative; bottom:0px"/>';
@@ -16,11 +18,7 @@
 	if(device.platform == 'iOS'){
 			element2.innerHTML = '<p onClick = "redirectIndex()" style="margin-left:3%; margin-top:5%; color:black; text-decoration: underline; font-size:2.5em">Retour</p>';
 	}
-	if(device.platform == 'iOS'){
-		element1.innerHTML = '<div style= "text-align:center; font-size:2em;" class="souscontainer"><p>Adresse : 6 Rue du Moyen Pont, 57000 Metz</p><p>Téléphone : 03 87 66 83 52</p><p>Horaires :  Ouvert 7/7j de 12:00 à 15:00 et de 18:30 à 23:00</p></div>'
-	}else{
-		element1.innerHTML = '<div style= "text-align:center; font-size:0.8em;" class="souscontainer"><p>Adresse : 6 Rue du Moyen Pont, 57000 Metz</p><p>Téléphone : 03 87 66 83 52</p><p>Horaires :  Ouvert 7/7j de 12:00 à 15:00 et de 18:30 à 23:00</p></div>';
-	}	
+        var element = document.getElementById('geolocation');				
 		lati = position.coords.latitude;
 		longi = position.coords.longitude ;
 			var map;
