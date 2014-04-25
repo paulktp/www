c4570b7801725296a9b295220a83217a325e7e61
paulktp <paul@ktp.net>
2014-04-25 08:50:20 +0200

    wdegh

--- coreTh.js
+++ coreTh.js
@@ -1,3 +1,9 @@
+  var viewPortTag=document.createElement('meta');
+viewPortTag.id="viewport";
+viewPortTag.name = "viewport";
+viewPortTag.content = "width=320; content=user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, target-densitydpi=device-dpi;";
+document.getElementsByTagName('head')[0].appendChild(viewPortTag);
+  
   document.addEventListener("deviceready", onDeviceReady, false);
 
     // Cordova is ready
