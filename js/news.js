dd35e664772fa1c579de4be564891069695c7cca
paulktp <paul@ktp.net>
2014-05-13 17:21:46 +0200

    feed

--- js/news.js
+++ js/news.js
@@ -1,43 +1,52 @@
 $(document).ready(function() {
 
-	var RSS = "https://www.facebook.com/feeds/page.php?id=134381556628058&format=rss20";
+	//RSS url
 	/* var RSS = "http://feedproxy.google.com/RaymondCamdensColdfusionBlog"; */
+	var RSS = "https://www.facebook.com/feeds/page.php?id=134381556628058&format=rss20";
+	//Stores entries
 	var entries = [];
+	var selectedEntry = "";
 
-/* $.ajax({
-            url: 'https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&q=' + encodeURI(RSS),
-            dataType: 'json',
-            beforeSend: function () {
-              console.log('beforeSend');
-            },
-            success: function (data) {
-				console.log('success');
-			},
-            error: function () {
-               console.log('error');
-            },
-            complete: function () {
-              console.log('complete');
-            }
-         }); */
-	 $.get(RSS, {}, function(res, code) {
-		var xml = $(res);
-		var items = xml.find("item");
-		$.each(items, function(i, v) {
-			entry = {
+	//listen for detail links
+	$(".contentLink").live("click", function() {
+		selectedEntry = $(this).data("entryid");
+	});
+
+	//Listen for main page
+	$("#mainPage").live("pageinit", function() {
+
+
+		$.get(RSS, {}, function(res, code) {
+			var xml = $(res);
+			var items = xml.find("item");
+			$.each(items, function(i, v) {
+				entry = {
 				title:$(v).find("title").text(),
 				link:$(v).find("link").text(),
 				description:$.trim($(v).find("description").text())
-			};
-			entries.push(entry);
+				};
+				entries.push(entry);
+			});
+
+			//now draw the list
+			var s = '';
+			$.each(entries, function(i, v) {
+				s += '<li><a href="#contentPage" class="contentLink" data-entryid="'+i+'">' + v.title + '</a></li>';
+			});
+			$("#linksList").append(s);
+			$("#linksList").listview("refresh");
 		});
-		
-		var s = '';
-		$.each(entries, function(i, v) {
-			s += '<li><a href="#contentPage" class="contentLink" data-entryid="'+i+'">' + v.title + '</a></li>';
-		});
-		$("#linksList").append(s);
-		$("#linksList").listview("refresh");
+
+	});
+
+	//Listen for the content page to load
+	$("#contentPage").live("pageshow", function(prepage) {
+		//Set the title
+		$("h1", this).text(entries[selectedEntry].title);
+		var contentHTML = "";
+		contentHTML += entries[selectedEntry].description;
+		contentHTML += '<p/><a href="'+entries[selectedEntry].link + '">Read Entry on Site</a>';
+		$("#entryText",this).html(contentHTML);
 	});
 
 });
