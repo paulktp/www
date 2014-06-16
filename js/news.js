5081f76a0fce99fab738fd50fc1e72cb9eff5ca6
paulktp <paul@ktp.net>
2014-06-16 10:38:49 +0200

    news date

--- js/news.js
+++ js/news.js
@@ -1,3 +1,22 @@
+var getPubDate = function(ladate)
+	{
+		var d = new Date(ladate);
+		var day = d.getDate();
+        var month = d.getMonth() + 1;
+        var year = d.getFullYear();
+		
+		if (day < 10) {
+            day = "0" + day;
+        }
+        if (month < 10) {
+            month = "0" + month;
+        }
+        var date = day + "/" + month + "/" + year;
+		
+		return date;
+	}
+
+
 $(document).ready(function() {
 
 	//RSS url
@@ -80,21 +99,5 @@ $(document).ready(function() {
 		}
     }
 	
-	var getPubDate = function(ladate)
-	{
-		var d = new Date(ladate);
-		var day = d.getDate();
-        var month = d.getMonth() + 1;
-        var year = d.getFullYear();
-		
-		if (day < 10) {
-            day = "0" + day;
-        }
-        if (month < 10) {
-            month = "0" + month;
-        }
-        var date = day + "/" + month + "/" + year;
-		
-		return date;
-	}
+	
 	
